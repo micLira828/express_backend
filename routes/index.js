a19971671b67026d2e8a3dbc13e71395e91fecbf
micLira828 <micgep1015@gmail.com>
2026-05-14 14:05:36 -0700

    csrf restore

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,15 @@ router.get('/hello/world', function(req, res) {
   res.send('Hello World!');
 });
 
+// backend/routes/index.js
+// ...
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+
 module.exports = router;
